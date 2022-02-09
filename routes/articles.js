f403f77a313626d336fe633a7e16f242296f2a62
Andrew Carroll <andrewc411@yahoo.com>
2022-02-09 15:05:45 -0500

    made adjustments to model, routes, and view

--- routes/articles.js
+++ routes/articles.js
@@ -36,9 +36,7 @@ function saveArticleAndRedirect(path) {
   return async (req, res) => {
     let article = req.article
     article.title = req.body.title
-    article.description = req.body.description
-    article.markdown = req.body.markdown
-    
+    article.description = req.body.description    
     try {
       article = await article.save()
       res.redirect(`/articles/${article.id}`)
